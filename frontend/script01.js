f6534e10becfe1ba4247d18d07a62c1cbddb2d09
Nickddb <nicollerocha.dev2024@gmail.com>
2025-04-15 15:52:47 -0300

    algo

--- frontend/script01.js
+++ frontend/script01.js
@@ -3,15 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function fetchProdutos (){
-    fetch("http://localhost:8000/api/produtos/")
+    fetch("http://127.0.0.1:8000/api/produtos/")
     .then(res => res.json())
     .then(data => renderProdutos(data))
     .catch(err => console.error("Erro ao buscar produtos", err));
 }
 
 function renderProdutos(produtos){
-
-    const container = document.getElementById("produtos-container");
+    const container = document.getElementById("Produtos-container");
     container.innerHTML = "";
 
     produtos.forEach(produto => {
@@ -22,7 +21,7 @@ function renderProdutos(produtos){
         <h2>${produto.nome}</h2>
         <p>${produto.descricao}</p>
         <p class="preco"> R$ ${produto.preco}</p>
-        <p><strong>Categoria>:</strong> ${produto.categoria.nome}</p>
+        <p><strong>categoria:</strong> ${produto.categoria.nome}</p>
     `;
     container.appendChild(card);
 });
